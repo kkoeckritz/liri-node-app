a001b4786a62503d63879e792dffc9c65785f511
Kris Koeckritz <kjk464@humboldt.edu>
2018-08-10 20:56:48 -0700

    integrate omdb, spotify

--- liri.js
+++ liri.js
@@ -1,26 +1,92 @@
-require("dotenv").config();
+// link required libs
 var fs = require("fs");
 var request = require("request");
+var Twitter = require("node-twitter-api");
+var Spotify = require("node-spotify-api");
 
 // grab key configs
+require("dotenv").config();
 var keys = require("./keys.js");
-// var spotify = new Spotfy(keys.spotify);
-// var client = new Twitter(keys.twitter);
+console.log("done.")
+
 
 function myTweets() {
+    var a_token = null;
+    var a_secret = null;
 
+    // get new instance of twitter ( THIS MIGHT BE ALL THAT IS NEEDED!!!!!!!!!!!!!!!!!! )
+    var twitter = new Twitter(keys.twitter);
+
+    // get request token
+    twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
+        if (error) {
+            console.log("Error getting OAuth request token : " + error);
+        }
+        else {
+            // get access token
+            twitter.getAccessToken(requestToken, requestTokenSecret, oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
+                if (error) {
+                    console.log(error);
+                }
+                else {
+                    a_token = accessToken;
+                    a_secret = accessTokenSecret;
+                }
+            });
+        }
+    });
 }
 
-function spotifyThisSong() {
+// Spotify mode
+function spotifyThisSong(name) {
+    var spotify = new Spotify(keys.spotify);
 
+    // default to "Ace of Base - The Sign"
+    if (name == null) {
+        name = "Ace of Base The Sign";
+    }
+
+    spotify.search({ type: 'track', query: name })
+        .then(function(response) {
+            console.log("____________________");
+            console.log(`artist: ${response.tracks.items[0].album.artists[0].name}`);
+            console.log(`track: ${response.tracks.items[0].name}`);
+            console.log(`album: ${response.tracks.items[0].album.name}`);
+            console.log(`Spotify link: ${response.tracks.items[0].external_urls.spotify}`);
+            console.log("____________________");
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 }
 
 function movieThis(name) {
+    // default to "Mr. Nobody"
+    if (name == null) {
+        name = "Mr. Nobody";
+    }
+
     request(`http://omdbapi.com/?t=${name}&y=&plot=short&apikey=${keys.omdb.key}`, function(error, response){
         if (!error && response.statusCode == 200) {
+            var movie_title = JSON.parse(response.body).Title;
+            var movie_year = JSON.parse(response.body).Year;
             var movie_rating = JSON.parse(response.body).imdbRating;
+            var movie_tomatoes = JSON.parse(response.body).Ratings[1].Source;
+            var movie_country = JSON.parse(response.body).Country;
+            var movie_language = JSON.parse(response.body).Language;
+            var movie_plot = JSON.parse(response.body).Plot;
+            var movie_actors = JSON.parse(response.body).Actors;
 
-            console.log(movie_rating);
+            console.log("____________________");
+            console.log(`title: ${movie_title}`);
+            console.log(`year: ${movie_year}`);
+            console.log(`rating: ${movie_rating}`);
+            console.log(`Rotten Tomatoes: ${movie_tomatoes}`);
+            console.log(`Country: ${movie_country}`);
+            console.log(`Language: ${movie_language}`);
+            console.log(`Plot: ${movie_plot}`);
+            console.log(`Actors: ${movie_actors}`);
+            console.log("____________________");
         }
         else {
             console.log(error);
@@ -29,7 +95,7 @@ function movieThis(name) {
 }
 
 function doWhatItSays() {
-
+    
 }
 
 // determine app mode from first parameter
@@ -40,7 +106,7 @@ switch(mode) {
         myTweets();
         break;
     case "spotify-this-song":
-        spotifyThisSong();
+        spotifyThisSong(name);
         break;
     case "movie-this":
         movieThis(name);
@@ -49,9 +115,9 @@ switch(mode) {
         doWhatItSays();
         break;
     default:
-        console.log("_____usage_____");
+        console.log("\n_____usage_____");
         console.log("'my-tweets'");
         console.log("'spotify-this-song <song>'");
         console.log("'movie-this <movie>'");
-        console.log("'do-what-it-says'");
+        console.log("'do-what-it-says'\n");
 }
