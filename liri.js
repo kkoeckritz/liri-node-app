0b9f05cc8de8a80f88e8de99b33ee26c8ceaf6ca
Kris Koeckritz <kjk464@humboldt.edu>
2018-08-11 00:07:46 -0700

    add logging functionality, fix Rotten Tomatoes bug

--- liri.js
+++ liri.js
@@ -7,7 +7,6 @@ var Spotify = require("node-spotify-api");
 // grab key configs
 require("dotenv").config();
 var keys = require("./keys.js");
-console.log("done.")
 
 // Twitter mode
 function myTweets() {
@@ -20,13 +19,13 @@ function myTweets() {
     // get request token
     twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
         if (error) {
-            console.log("Error getting OAuth request token : " + error);
+            logOut(1,"Error getting OAuth request token : " + error);
         }
         else {
             // get access token
             twitter.getAccessToken(requestToken, requestTokenSecret, oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
                 if (error) {
-                    console.log(error);
+                    logOut(1,error);
                 }
                 else {
                     a_token = accessToken;
@@ -49,15 +48,13 @@ function spotifyThisSong(name) {
     // find, output data from Spotify
     spotify.search({ type: 'track', query: name })
         .then(function(response) {
-            console.log("____________________");
-            console.log(`artist: ${response.tracks.items[0].album.artists[0].name}`);
-            console.log(`track: ${response.tracks.items[0].name}`);
-            console.log(`album: ${response.tracks.items[0].album.name}`);
-            console.log(`Spotify link: ${response.tracks.items[0].external_urls.spotify}`);
-            console.log("____________________");
+            logOut(1,`artist: ${response.tracks.items[0].album.artists[0].name}`);
+            logOut(1,`track: ${response.tracks.items[0].name}`);
+            logOut(1,`album: ${response.tracks.items[0].album.name}`);
+            logOut(1,`Spotify link: ${response.tracks.items[0].external_urls.spotify}`);
         })
         .catch(function(err) {
-            console.log(err);
+            logOut(1,err);
         });
 }
 
@@ -75,25 +72,31 @@ function movieThis(name) {
             var movie_title = JSON.parse(response.body).Title;
             var movie_year = JSON.parse(response.body).Year;
             var movie_rating = JSON.parse(response.body).imdbRating;
-            var movie_tomatoes = JSON.parse(response.body).Ratings[1].Source;
+
+            // Rotten Tomatoes rating sometimes does not exist
+            if (JSON.parse(response.body).Ratings[1] == undefined) {
+                var movie_tomatoes = "NOT FOUND.";
+            }
+            else {
+                var movie_tomatoes = JSON.parse(response.body).Ratings[1].Source;
+            }
+            
             var movie_country = JSON.parse(response.body).Country;
             var movie_language = JSON.parse(response.body).Language;
             var movie_plot = JSON.parse(response.body).Plot;
             var movie_actors = JSON.parse(response.body).Actors;
 
-            console.log("____________________");
-            console.log(`title: ${movie_title}`);
-            console.log(`year: ${movie_year}`);
-            console.log(`rating: ${movie_rating}`);
-            console.log(`Rotten Tomatoes: ${movie_tomatoes}`);
-            console.log(`Country: ${movie_country}`);
-            console.log(`Language: ${movie_language}`);
-            console.log(`Plot: ${movie_plot}`);
-            console.log(`Actors: ${movie_actors}`);
-            console.log("____________________");
+            logOut(1,`title: ${movie_title}`);
+            logOut(1,`year: ${movie_year}`);
+            logOut(1,`rating: ${movie_rating}`);
+            logOut(1,`Rotten Tomatoes: ${movie_tomatoes}`);
+            logOut(1,`Country: ${movie_country}`);
+            logOut(1,`Language: ${movie_language}`);
+            logOut(1,`Plot: ${movie_plot}`);
+            logOut(1,`Actors: ${movie_actors}`);
         }
         else {
-            console.log(error);
+            logOut(1,error);
         }
     });
 }
@@ -101,12 +104,12 @@ function movieThis(name) {
 // run inputs from random.txt instead
 function doWhatItSays() {
 
-    console.log("Doing what it says...");
+    console.log("Doing what it says...\n");
     fs.readFile("random.txt", "utf8", function(error, data) {
 
         // log any errors to console
         if (error) {
-          return console.log(error);
+          return logOut(1,error);
         }
       
         // split "paramaters" at comma
@@ -121,24 +124,70 @@ function doWhatItSays() {
 function chooseMode(mode, name) {
     switch(mode) {
         case "my-tweets":
+            // log commands with logOut
+            logOut(0, `${mode}`);
+
             myTweets();
             break;
+
         case "spotify-this-song":
+            // log commands with logOut
+            logOut(0, `${mode} ${name}`);
+
             spotifyThisSong(name);
             break;
+
         case "movie-this":
+            // log commands with logOut
+            logOut(0, `${mode} ${name}`);
+
             movieThis(name);
             break;
+
         case "do-what-it-says":
+            // log commands with logOut
+            logOut(0, `${mode}`);
+
             doWhatItSays();
             break;
+
         default:
-            console.log("_____usage_____");
-            console.log("'my-tweets'");
-            console.log("'spotify-this-song <song>'");
-            console.log("'movie-this <movie>'");
-            console.log("'do-what-it-says'");
-            console.log("_______________");
+            logOut(1, "_____usage_____");
+            logOut(1, "'my-tweets'");
+            logOut(1, "'spotify-this-song <song>'");
+            logOut(1, "'movie-this <movie>'");
+            logOut(1, "'do-what-it-says'");
+    }
+}
+
+// write to log.txt, console
+function logOut(type, text) {
+
+    // for commands
+    if (type == 0) {
+
+        // write text to file...
+        fs.appendFile("log.txt", `${text}:\n`, function(err) {
+
+            if (err) {
+            console.log(err);
+            }
+        });             
+    }
+
+    // for output
+    else {
+
+        // write text to file...
+        fs.appendFile("log.txt", `\t${text}\n`, function(err) {
+
+            if (err) {
+            console.log(err);
+            }
+        
+            // ...and output text to console 
+            console.log(text);
+        });       
     }
 }
 
